4dcb2febdd6871ecf05833d2b8c3f906813a62dc
Devin Baysa <baysa@oakland.edu>
2025-02-14 20:30:37 -0500

    Creating program

---
+++ App.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+
+public class App {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a string: ");
+        String input = scanner.nextLine();
+        String reversed = reverseString(input);
+        System.out.println("Reversed string: " + reversed);
+        scanner.close();
+    }
+
+    public static String reverseString(String input) {
+        if (input == null) {
+            return null;
+        }
+        char[] chars = input.toCharArray();j 
+        int left = 0;
+        int right = chars.length - 1;
+        while (left < right) {
+            char temp = chars[left];
+            chars[left] = chars[right];
+            chars[right] = temp;
+            left++;
+            right--;
+        }
+        return new String(chars);
+    }
+}
